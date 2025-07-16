126663654e2bf37a4be5890d69a2b00bb8d60ee6
neelam1424 <neelammore1404@gmail.com>
2025-07-16 17:52:42 +0530

    NavBar

--- src/components/student/Navbar.jsx
+++ src/components/student/Navbar.jsx
@@ -10,16 +10,23 @@ const Navbar = () => {
 
 
   return (
-    <div className={`flex items-center justify-betwwen px-4 sm:px-10 md:px-14 lg:px-36 border-b border-gray-500 py-4 ${isCourseListPage ? 'bg-whitw' :'bg-cyan-100/70'}`}>
+    <div className={`flex items-center justify-between px-4 sm:px-10 md:px-14 lg:px-36 border-b border-gray-500 py-4 ${isCourseListPage ? 'bg-white' :'bg-cyan-100/70'}`}>
         <img src={assets.logo} alt="Logo" className='w-28 lg:w-32 cursor-pointer' />
         <div className='hidden md:flex item-center gap-5 text-gray-500'>
-            <div>
+            <div className='flex items-center gap-5'>
                 <button>become Educator</button>
                 <Link to='/my-enrollments'>My Enrollments</Link>
             </div>
             <button className='bg-blue-600 text-white px-5 py-2 rounded-full'>Create Account</button>
         </div>
-        <div>
+
+        {/*For Phone Screens */}
+        <div className='md:hidden flex items-center gap-2 sm:gap-5 text-gray-500'>
+            <div>
+                <button>become Educator</button>
+                <Link to='/my-enrollments'>My Enrollments</Link>
+                <button><img src={assets.user_icon} alt="" /></button>
+            </div>
         </div>        
     </div>
   )
