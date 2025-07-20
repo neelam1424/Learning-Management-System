7b911c50c5b148648512aa3becb84507ad65c99e
neelam1424 <neelammore1404@gmail.com>
2025-07-20 23:03:32 +0530

    Testimonial

--- src/components/student/Navbar.jsx
+++ src/components/student/Navbar.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { assets } from '../../assets/assets'
 import { Link } from 'react-router-dom'
 import { useClerk ,UserButton,useUser } from '@clerk/clerk-react'
+import { AppContext } from '../../context/AppContext'
 
 const Navbar = () => {
+
+    const {navigate,isEducator}=useContext(AppContext)
 
     const isCourseListPage=location.pathname.includes('/course-list');
 
@@ -14,11 +17,11 @@ const {user}=useUser();
 
   return (
     <div className={`flex items-center justify-between px-4 sm:px-10 md:px-14 lg:px-36 border-b border-gray-500 py-4 ${isCourseListPage ? 'bg-white' :'bg-cyan-100/70'}`}>
-        <img src={assets.logo} alt="Logo" className='w-28 lg:w-32 cursor-pointer' />
+        <img onClick={()=> navigate('/')} src={assets.logo} alt="Logo" className='w-28 lg:w-32 cursor-pointer' />
         <div className='hidden md:flex item-center gap-5 text-gray-500'>
             <div className='flex items-center gap-5'>
                 { user && <>
-                <button>Become Educator</button>
+                <button onClick={()=>{navigate('/educator')}}>{isEducator? 'Educator Dashboard' : 'become Educator'}</button>
                 | <Link to='/my-enrollments'>My Enrollments</Link>
                 </>
                 }
@@ -32,7 +35,7 @@ const {user}=useUser();
             <div className='flex iems-center gap-1sm:gap-2 max-sm:text-xs'>
                 
                  { user && <>
-                <button>Become Educator</button>
+                <button onClick={()=>{navigate('/educator')}}>{isEducator? 'Educator Dashboard' : 'become Educator'}</button>
                 | <Link to='/my-enrollments'>My Enrollments</Link>
                 </>
                 }
--- src/components/student/TestimonialsSection.jsx
+++ src/components/student/TestimonialsSection.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import { dummyTestimonial } from '../../assets/assets'
 
 const TestimonialsSection = () => {
   return (
-    <div>TestimonialsSection</div>
+    <div className='pb-14 px-8 md:px-0'>
+      <h2 className='text-3xl font-medium text-gray-800'>Testimonials</h2>
+      <p className='md:text-base text-gray-500 mt-3'>Hear from our learners as they share their journeys of transformation,sucess, and how our <br />platform has made a difference in their lives.</p>
+      <div>
+        {dummyTestimonial.map((testimonial,index)=>(
+          <div key={index}>
+            <div>
+              <img src={testimonial.image} alt={testimonial.name} />
+              <div>
+                <h1>{testimonial.name}</h1>
+                <p>{testimonial.role}</p>
+              </div>
+              <div>
+                <div>
+                  {[...Array(5).map((_, i)=>(
+                    <img className='h-5' key={i} src={i < Math.floor(testimonial.rating)? assets.star :asserts.star_blank} alt="star" />
+                  ))]}
+                </div>
+                <p className='text-gray-500 mt-5'>{testimonial.feedback}</p>
+              </div>
+            </div>
+          </div>
+         
+        ))}
+      </div>
+    </div>
   )
 }
 
