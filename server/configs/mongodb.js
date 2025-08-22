c222b4a094aa069fe8793bd61eb96c228c4efb38
neelam1424 <neelammore1404@gmail.com>
2025-08-22 22:29:52 +0530

    Fixed nested git repositories

---
+++ server/configs/mongodb.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+//Connect to the mongoDB database
+
+
+const connectDB = async ()=>{
+    mongoose.connection.on('connected',()=> console.log('Database Connected'))
+
+    await mongoose.connect(`${process.env.MONGODB_URL}/lms`)
+}
+
+export default connectDB
