f93e656236a22c015c577513e75c6e771f3886e8
neelam1424 <neelammore1404@gmail.com>
2025-08-29 18:00:39 -0400

    Project Completed Succesfully

--- client/src/pages/student/MyEnrollments.jsx
+++ client/src/pages/student/MyEnrollments.jsx
@@ -1,4 +1,4 @@
-import React, {useContext, useState} from 'react'
+import React, {useContext, useEffect, useState} from 'react'
 import { AppContext } from '../../context/AppContext'
 import {Line} from 'rc-progress'
 import Footer from '../../components/student/Footer'
