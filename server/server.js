6f227c6d3df7d4bbbc174b3a6da2821657da45e5
neelam1424 <neelammore1404@gmail.com>
2025-08-30 13:38:01 -0400

    server frontend link update

--- server/server.js
+++ server/server.js
@@ -20,6 +20,7 @@ const PORT = process.env.PORT || 5000;
 const allowedOrigins = [
   'http://localhost:5173',
   'https://learning-management-system-frontend-psi-six.vercel.app',
+  'learning-management-system-frontend-n9ki9nswy.vercel.app'
 ];
 
 app.use(cors({
